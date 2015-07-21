9b86186f11fc36ecde32b4a456ad0f25d1402fe0
Thomas Riedl <github@riedl.cyancor.com>
2015-07-21 19:32:34 +0200

    Cleanup

--- TimeSheetFilters/TimeSheetFilters.js
+++ TimeSheetFilters/TimeSheetFilters.js
@@ -1,37 +1,56 @@
 // Author: Thomas Riedl - CyanCor GmbH - https://cyancor.com/
 tau.mashups
-  .addDependency('libs/jquery/jquery')
-  .addMashup(function($, config) {
-	  
-    var TimeSheetFilters = 
+.addDependency('libs/jquery/jquery')
+.addMashup(function($, config)
+{
+	'use strict';
+
+	$(document).ready(function()
 	{
-		Initialize : function()
+		var TimeSheetFilters = 
 		{
-			this.UpdateAllInputs();
-		},
-		
-		UpdateAllInputs : function()
-		{
-			$('td .tC input[value!=0]').css('color', '#000');
-			$('td .tC input[value=0]').css('color', '#FFF');
-		}
-	};
-	  
-    function updateInput()
-    {
-        var element = $(this);
-        var value = element.val();
-        element.css('color', value && value != '0' ? '#000' : '#FFF');
-    }
-     
-    var timeSheetFilters = Object.create(TimeSheetFilters);
-    timeSheetFilters.Initialize();
-	
-    var topBar = $('<tr></tr>');
-    var cell = $('<td colspan="4" align="right"></td>');
-    var updateButton = $('<input type="button" value="Update" />');
-    updateButton.click(timeSheetFilters.UpdateAllInputs);
-    cell.append(updateButton);
-    topBar.append(cell);
-    $('#timeOptions > tbody').prepend(topBar);
-  });
+			Initialize : function()
+			{
+				this.UpdateUI();
+				this.AttachControls();
+				
+        		var $target = $('#ctl00_mainArea_pnlUpd');
+        		if (!$target.length)
+        		{
+        			return;
+        		}
+        		if (window.MutationObserver)
+        		{
+        		    var self = this;
+        			var observer = new MutationObserver(function() { self.UpdateUI.call(self); });
+        			observer.observe($target[0], { childList: true });
+        		}
+			},
+			
+			UpdateUI: function()
+			{
+				this.UpdateAllInputs();
+			},
+			
+			UpdateAllInputs: function()
+			{
+				$('td .tC input[value!=0]').css('color', '#000');
+				$('td .tC input[value=0]').css('color', '#FFF');
+			},
+			
+			AttachControls: function()
+			{
+        		var topBar = $('<tr></tr>');
+        		var cell = $('<td colspan="4" align="right"></td>');
+        		var updateButton = $('<input type="button" value="Update" />');
+        		updateButton.click(timeSheetFilters.UpdateAllInputs);
+        		cell.append(updateButton);
+        		topBar.append(cell);
+        		$('#timeOptions > tbody').prepend(topBar);
+			}
+		};
+		  
+		var timeSheetFilters = Object.create(TimeSheetFilters);
+		timeSheetFilters.Initialize();
+	});
+});
